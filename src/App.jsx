ae50293e8c97926f41a0ec35c599903608ee8ea2
Yagmur Caglar <yagmurrbeyza@gmail.com>
2024-03-15 02:26:09 +0300

    sayfalarOluşturuldu

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import workintech from '/workintech.svg'
-import './App.css'
+ import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min'
+import Siparis from './components/Siparis';
+import Home from './components/Home';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+
     <>
-      <div>
-        <a href="https://github.com/Workintech/fsweb-s7-challenge-pizza" target="_blank">
-          <img src={workintech} className="logo" alt="Workintech logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Workintech + 🍕</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Absolute Acı Pizza sayısı {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Workintech or Pizza logos to learn more
-      </p>
+    <Switch>
+
+    <Route exact path="/" >
+    <Home/>
+          </Route>   
+          <Route exact path="/siparis">
+         <Siparis />
+          </Route>            
+      </Switch>  
     </>
   )
-}
+}                      
 
-export default App
+export default App;                                                                                                                                                                                                          
---
+++ src/components/Siparis.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import PizzaOrderForm from './PizzaOrderForm'
+
+
+
+function Siparis() {
+    return (
+        <section>
+
+        <div>
+            <h1>Position Absolute Acı Pizza</h1>
+            <p>Frontent Dev olarak hala position:absolute kullanıyorsan bu çok aci pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak, düzleştirilmiş mayali buğday bazlı hamurdan oluşan İtalyan kökenli lezzetli bir yemektir. Küçük bir pizzaya bazen pizzetta denir.</p>
+           
+        </div>
+        <div>
+      <PizzaOrderForm/>
+        </div>
+        </section> 
+              
+    )
+}
+
+export default Siparis
